9f3511a3196f7ed5b1a6288f2afa5843c5095ca1
Nargis Fatime <nargis.fatima160@gmail.com>
2023-12-09 23:21:31 +0530

    javascript

---
+++ javascript/form.js
@@ -0,0 +1,8 @@
+function msg(data){
+    if(data.toString().includes("https://web-development-sigma-nine.vercel.app/html/form.html")){
+        window.location.href="https://web-development-sigma-nine.vercel.app/"
+    }
+    else{
+        window.location.href="http://127.0.0.1:5500"
+    }
+}
